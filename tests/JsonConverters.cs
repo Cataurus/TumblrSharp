fb41581a1ed1f11799544e4dfdbf319d3f490c00
Ulf Martin Prill <u.prill@intero-technologies.de>
2025-05-16 08:20:18 +0200

    Tests

--- tests/JsonConverters.cs
+++ tests/JsonConverters.cs
@@ -122,6 +122,8 @@ namespace TestingTumblrSharp
         [TestMethod]
         public async Task TrailConverter()
         {
+            Console.WriteLine("ConsumerKey: " + CONSUMERKEY);
+
             TumblrClient tc = new TumblrClientFactory().Create<TumblrClient>(_consumerKey, _consumerSecret, new Token(_accessKey, _accessSecret));
 
             Assert.IsNotNull(tc);
